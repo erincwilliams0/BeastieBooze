42ccecebcb7f05c1c8635d7597d98da448373bd7
Erin Williams <erin.cwilliams0@gmail.com>
2022-07-26 16:47:27 -0500

    initial setup updated some dependencies

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { DB_USERNAME, DB_PASSWORD } = process.env;
-const mongoUri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.r3e81.mongodb.net/beastie-booze`;
+const mongoUri = process.env.ATLAS_URL;
 console.log({ DB_USERNAME, DB_PASSWORD });
 
 const db = mongoose.connect(mongoUri, {
