9f81e9b3ffacf35d315c26a6b05c504deff085ec
Erin Williams <erin.cwilliams0@gmail.com>
2022-07-27 20:07:04 -0500

    working on Shopping List component: TableHead initial setup

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -1,13 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+import { useTheme } from '@mui/material/styles';
 
+// columns with a field of drinkId, drink name, quantity;
+const columns = [
+  { id: 'id', label: 'ID' },
+  { id: 'name', label: 'Drink' },
+  { id: 'quantity', label: 'Quantity' }
+]
 
+function createData(id, name, quantity) {
+  return { id, name, quantity };
+}
+
+const rows = [
+  createData(22, "Jack and coke", 5),
+  createData(13, "redbull and vodka", 2),
+  createData(40, "Jack Daniels Float", 10),
+  createData(5, "hand grenade", 8),
+  createData(62, "Shark Attack", 5),
+  createData(37, "JaegarBombs", 11),
+  createData(70, "Irish Car Bombs", 1)
+]
 
 const ShoppingList = () => {
 
 
 
   return (
-    <div>shopping list here!!!</div>
+    <TableContainer component={Paper} >
+      <Table stickyHeader >
+        <TableHead>
+          <TableRow>
+
+            {columns.map((column) => {
+              <TableCell 
+              key={column.id} > {column.label} </TableCell> })
+            }
+
+          </TableRow>
+        </TableHead>
+      </Table>
+    </TableContainer>
   )
 }
 
