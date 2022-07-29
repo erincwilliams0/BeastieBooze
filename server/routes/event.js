7175a4027b647728e4beb41d1600ccbb21cca685
dow2 <dowedwards@gmail.com>
2022-07-29 09:59:51 -0500

    added routes, and frontend comonents

--- server/routes/event.js
+++ server/routes/event.js
@@ -2,8 +2,8 @@ const { Router } = require('express');
 
 const eventRouter = Router();
 
-eventRouter.get('/event', (req, res) => {
-  Events.findOne({ _id: req.query.id })
+eventRouter.get('/events', (req, res) => {
+  Event.findOne()
     .then((data) => res.status(200).send(data))
     .catch((err) => res.sendStatus(500));
 });
